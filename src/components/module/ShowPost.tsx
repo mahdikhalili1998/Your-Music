2f0dc8674b26699f884a060c70f338dc80231eb5
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-09-22 15:14:16 +0330

    workimg on main page

--- src/components/module/ShowPost.tsx
+++ src/components/module/ShowPost.tsx
@@ -3,12 +3,17 @@
 import { IShowPost } from "@/types/props";
 import Image from "next/image";
 import { FC, useEffect, useState } from "react";
+import { p2e } from "@/helper/replaceNumber.js";
+import moment from "moment";
 
 const ShowPost: FC<IShowPost> = ({ post }) => {
   return (
     <div>
       {post.map((item) => (
-        <div key={item._id} className="mx-1 flex flex-col gap-5 font-iransans">
+        <div
+          key={item._id}
+          className="mx-1 mt-4 flex flex-col gap-5 border-b-2 border-solid border-gray-400 pb-4 font-iransans"
+        >
           <div className="flex items-center justify-start gap-3">
             <Image
               src={item.profilePicUrl}
@@ -24,6 +29,9 @@ const ShowPost: FC<IShowPost> = ({ post }) => {
             <source src={item.musicUrl} type="audio/mp3" />
           </audio>
           <p className="font-medium text-p-950">{item.description}</p>
+          <p className="-mt-4 text-xs font-medium text-gray-600">
+            {moment(item.createdAt).format("YYYY/MM/DD")}
+          </p>
         </div>
       ))}
     </div>
