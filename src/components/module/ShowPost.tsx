f61a5de24793b7791334c58376591b797d615fd0
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-10-04 19:11:11 +0330

    work on detail

--- src/components/module/ShowPost.tsx
+++ src/components/module/ShowPost.tsx
@@ -20,11 +20,11 @@ import momentJalaali from "moment-jalaali";
 import toast, { Toaster } from "react-hot-toast";
 
 const ShowPost: FC<IShowPost> = ({ post, info, user, locale }) => {
+  // console.log({ post, info, user });
   const [activePostId, setActivePostId] = useState<string | null>(null);
   const [likeState, setLikeState] = useState<{ [key: string]: boolean }>({});
   const [likeCount, setLikeCount] = useState<{ [key: string]: number }>({});
   const [loading, setLoading] = useState<{ [key: string]: boolean }>({});
-  const jalaliDate = momentJalaali(post.createAt).format("jYYYY/jMM/jDD");
   const [profPic, setProfPic] = useState<string>("");
   const reversedPost = post.toReversed();
   const refs = useRef<{ [key: string]: HTMLDivElement | null }>({});
@@ -100,92 +100,99 @@ const ShowPost: FC<IShowPost> = ({ post, info, user, locale }) => {
 
   return (
     <div>
-      {reversedPost.map((item) => (
-        <div
-          key={item._id}
-          className={`${locale === "fa" ? "directon-ltr font-iransans" : null} mx-1 mt-4 flex flex-col gap-5 border-b-2 border-solid border-gray-400 pb-4 md:mx-10 lg:mx-20`}
-        >
-          <div className="flex items-center justify-between">
-            <div className="flex items-center justify-start gap-3">
-              <Image
-                src={item.profilePicUrl}
-                alt="profilePicture"
-                width={300}
-                height={300}
-                priority
-                className="size-[4rem] rounded-[100%] border-2 border-solid border-p-700"
-              />
-              <span className="font-medium text-p-950">{item.userName}</span>
-            </div>
-            <div className="relative">
-              <SlOptionsVertical
-                className="-mt-1"
-                onClick={() =>
-                  setActivePostId(activePostId === item._id ? null : item._id)
-                }
-              />
-              <div
-                ref={(el) => (refs.current[item._id] = el)}
-                className={`${
-                  activePostId !== item._id ? "hidden" : "absolute right-1"
-                } ${locale === "fa" ? "px-6" : null} z-10 flex flex-col items-start justify-center gap-2 rounded-lg bg-gray-300 p-2 px-4 font-medium`}
-              >
-                {!user ? null : user.userName === item.userName ? (
-                  <span
-                    onClick={() => deleteHandler(item._id)}
-                    className="flex items-center gap-2 text-red-600"
-                  >
-                    <AiFillDelete /> {t("delete")}
-                  </span>
-                ) : null}
-                <a
-                  href={item.musicUrl}
-                  className="flex w-max items-center gap-2"
-                >
-                  <LuDownload /> {t("download")}
-                </a>
-              </div>
-            </div>
-          </div>
-          <audio controls className="mx-2 mb-4 w-full 350:mx-auto 350:w-64">
-            <source src={item.musicUrl} type="audio/mp3" />
-          </audio>
-          <div className="flex items-center gap-4">
-            {loading[item._id] ? ( // نمایش لودر فقط برای پست در حال لایک شدن
-              <div>
-                <Loader color="#020617" width={40} height={20} />
-              </div>
-            ) : (
-              <span
-                onClick={() => likeHandler(item._id)}
-                className="flex items-center gap-2"
-              >
-                {likeState[item._id] ? (
-                  <FaHeart className="text-xl text-red-600" />
-                ) : (
-                  <FaRegHeart className="text-xl" />
-                )}
-                {likeCount[item._id]}
-              </span>
-            )}
-            <span>
-              <FaRegComment className="text-xl" />
-            </span>
-          </div>
-          <p
-            className={`${
-              isPersian(item.description) ? "font-iransans" : "font-Roboto"
-            } font-medium text-p-950`}
+      {reversedPost.map((item) => {
+        const userInfo = info.find((user) => user._id === item.userId);
+        return (
+          <div
+            key={item._id}
+            className={`${locale === "fa" ? "directon-ltr font-iransans" : null} mx-1 mt-4 flex flex-col gap-5 border-b-2 border-solid border-gray-400 pb-4 md:mx-10 lg:mx-20`}
           >
-            {item.description}
-          </p>
-          <p className="-mt-4 text-xs font-medium text-gray-600">
-            {locale === "fa"
-              ? jalaliDate
-              : p2e(moment(post.createdAt).format("YYYY/MM/DD"))}
-          </p>
-        </div>
-      ))}
+            <div className="flex items-center justify-between">
+              {userInfo && (
+                <div className="flex items-center justify-start gap-3">
+                  <Image
+                    src={userInfo.profilePicUrl}
+                    alt="profilePicture"
+                    width={300}
+                    height={300}
+                    priority
+                    className="size-[4rem] rounded-[100%] border-2 border-solid border-p-700"
+                  />
+                  <span className="font-medium text-p-950">
+                    {userInfo.userName}
+                  </span>
+                </div>
+              )}
+              <div className="relative">
+                <SlOptionsVertical
+                  className="-mt-1"
+                  onClick={() =>
+                    setActivePostId(activePostId === item._id ? null : item._id)
+                  }
+                />
+                <div
+                  ref={(el) => (refs.current[item._id] = el)}
+                  className={`${
+                    activePostId !== item._id ? "hidden" : "absolute right-1"
+                  } ${locale === "fa" ? "px-6" : null} z-10 flex flex-col items-start justify-center gap-2 rounded-lg bg-gray-300 p-2 px-4 font-medium`}
+                >
+                  {!user ? null : user.userName === item.userName ? (
+                    <span
+                      onClick={() => deleteHandler(item._id)}
+                      className="flex items-center gap-2 text-red-600"
+                    >
+                      <AiFillDelete /> {t("delete")}
+                    </span>
+                  ) : null}
+                  <a
+                    href={item.musicUrl}
+                    className="flex w-max items-center gap-2"
+                  >
+                    <LuDownload /> {t("download")}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <audio controls className="mx-2 mb-4 w-full 350:mx-auto 350:w-64">
+              <source src={item.musicUrl} type="audio/mp3" />
+            </audio>
+            <div className="flex items-center gap-4">
+              {loading[item._id] ? ( // نمایش لودر فقط برای پست در حال لایک شدن
+                <div>
+                  <Loader color="#020617" width={40} height={20} />
+                </div>
+              ) : (
+                <span
+                  onClick={() => likeHandler(item._id)}
+                  className="flex items-center gap-2"
+                >
+                  {likeState[item._id] ? (
+                    <FaHeart className="text-xl text-red-600" />
+                  ) : (
+                    <FaRegHeart className="text-xl" />
+                  )}
+                  {likeCount[item._id]}
+                </span>
+              )}
+              <span>
+                <FaRegComment className="text-xl" />
+              </span>
+            </div>
+            <p
+              className={`${
+                isPersian(item.description) ? "font-iransans" : "font-Roboto"
+              } font-medium text-p-950`}
+            >
+              {item.description}
+            </p>
+            <p className="-mt-4 text-xs font-medium text-gray-600">
+              {locale === "fa"
+                ? momentJalaali(item.createdAt).format("jYYYY/jMM/jDD")
+                : p2e(moment(item.createdAt).format("YYYY/MM/DD"))}
+            </p>
+          </div>
+        );
+      })}
       <Toaster />
     </div>
   );
