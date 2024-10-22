a998d2cd30ab471c66e64394f77bbaaed96a37a1
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-10-22 11:21:48 +0330

    file added

--- src/app/[locale]/notFound/page.tsx
+++ src/app/[locale]/notFound/page.tsx
@@ -76,3 +76,25 @@ function PageNotFound() {
 }
 
 export default PageNotFound;
+
+// "use client";
+// import Loader from "@/components/module/Loader";
+// import { useParams, useRouter } from "next/navigation";
+// import { useEffect } from "react";
+
+// const NotFoundPage = () => {
+//   const router = useRouter();
+//   const result = useParams();
+//   console.log(result);
+//   useEffect(() => {
+//     router.push("/en/notFound");
+//   }, []);
+
+//   return (
+//     <div className="w-max mx-auto ">
+//       <Loader color="#7e22ce" width={180} height={100} />
+//     </div>
+//   );
+// };
+
+// export default NotFoundPage;
