68b72f8010c6eeb24c69e9595cab536f28ec9f10
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-11-15 14:40:35 +0330

    add monifest and some meta data

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,11 +9,6 @@ import NextAuthProvider from "@/providers/NextAuthProvider";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 
-export const metadata: Metadata = {
-  title: "Your Music",
-  description: "Upload Your Favorite",
-};
-
 interface LayoutProps {
   children: React.ReactNode;
   params: { locale: string };
@@ -28,8 +23,31 @@ const RootLayout: React.FC<LayoutProps> = async ({
   return (
     <html lang={locale} dir={locale === "fa" ? "rtl" : "ltr"}>
       <head>
+        <title>Your Music</title>
+        <meta name="description" content="Upload Your Favorite" />
+        {/* safari  */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Upload Your Favorite"
+        />
+        <link rel="apple-touch-icon" sizes="48×48" href="icon/48.png" />
+        <link rel="apple-touch-icon" sizes="64×64" href="icon/64.png" />
+        <link rel="apple-touch-icon" sizes="96×96" href="icon/96.png" />
+        <link rel="apple-touch-icon" sizes="144×144" href="icon/144.png" />
+        <link rel="apple-touch-icon" sizes="180×180" href="icon/180.png" />
+        <link rel="apple-touch-icon" sizes="192×192" href="icon/192.png" />
+        <link rel="apple-touch-icon" sizes="512×512" href="icon/512.png" />
+        {/* end safari */}
+        {/* ie */}
+        <meta name="msapplication-TileImage" content="/icon/144.png" />
+        <meta name="msapplication-TileColor" content="transparent" />
+        <meta name="msapplication-starturl" content="/" />
+        {/* end ie  */}
+        <meta name="theme-color" content="transparent" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
-        {/* دیگر تگ‌های head می‌توانند اینجا قرار گیرند */}
       </head>
       <body className={"mx-auto max-w-[1600px] font-Roboto"}>
         <NextIntlClientProvider messages={messages}>
