d65bfd779b94dcccade9e48a49d41fcdf47cddff
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-11-22 19:11:08 +0330

    responsiving install app

--- src/components/template/InstallApp.tsx
+++ src/components/template/InstallApp.tsx
@@ -13,7 +13,7 @@ const InstallApp: FC<IInstallApp> = ({
   return (
     showInstallModal && (
       <div
-        className={`${locale === "fa" ? "directon-rtl" : "directon-ltr"} absolute z-40 flex flex-col items-center justify-center gap-5 rounded-xl bg-p-700 px-10 py-5`}
+        className={`${locale === "fa" ? "directon-rtl" : "directon-ltr"} 700:right-28 absolute z-40 flex flex-col items-center justify-center gap-5 rounded-xl bg-gradient-to-r from-p-700 to-p-400 px-10 py-5 500:right-3 550:right-8 600:left-7 600:top-24 sm:right-16 sm:w-max md:right-40 lg:right-72 xl:right-96`}
       >
         <h3
           className={`${locale === "fa" ? "font-iransans text-sm" : "font-Roboto"} text-white`}
